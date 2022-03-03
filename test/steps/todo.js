7d025453602166f18297775085c4784f3d284aa4
Furkan Küçük <furkan.kucuk@trhqmac150.local>
2022-03-03 09:53:52 +0300

    acceptance start

--- test/steps/todo.js
+++ test/steps/todo.js
@@ -1,7 +1,23 @@
-const { When, Then, Given } = require("cucumber");
+const { When, Then, Given, Before } = require("cucumber");
+var chai = require('chai')
+const webdriver = require('selenium-webdriver');
 
-Given('Empty ToDo list', function() {
+initDriver = () => {
+    driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .build();
+    return driver;
+}
 
+   Before(function() {
+        driver = initDriver()
+    })
+
+Given('Empty ToDo list', async function() {
+
+    await driver.get('http://192.168.1.101:8080/')
+  
+    await expect(driver.element.all(by.xpath("//*[@class='background-todoElement']")).isPresent()).toBe(false);
 });
 
 When('I write {string} to text box and click to add button', function(string) {
@@ -12,65 +28,65 @@ Then('I should see {string} item in ToDo list', function(string) {
 
 });
 
-Given('ToDo list with {string} item', function(string) {
+// Given('ToDo list with {string} item', function(string) {
 
-});
+// });
 
-When('I write {string} to text box and click to add button', function(string) {
+// When('I write {string} to text box and click to add button', function(string) {
 
-});
+// });
 
-Then('I should see {string} insterted to ToDo list below {string}', function(string, string2) {
+// Then('I should see {string} insterted to ToDo list below {string}', function(string, string2) {
 
-});
+// });
 
-Given('ToDo list with {string} item', function(string) {
+// Given('ToDo list with {string} item', function(string) {
 
-});
+// });
 
-When('I click on {string} text', function(string) {
+// When('I click on {string} text', function(string) {
 
-});
+// });
 
-Then('I should see {string} item marked as {string}', function(string, string2) {
+// Then('I should see {string} item marked as {string}', function(string, string2) {
 
-});
+// });
 
-Given('ToDo list with marked {string} item', function(string) {
+// Given('ToDo list with marked {string} item', function(string) {
 
-});
+// });
 
-When('I click on {string} text', function(string) {
+// When('I click on {string} text', function(string) {
 
-});
+// });
 
-Then('I should see mark of {string} item should be cleared as {string}', function(string, string2) {
+// Then('I should see mark of {string} item should be cleared as {string}', function(string, string2) {
 
-});
+// });
 
-Given('ToDo list with {string} item', function(string) {
+// Given('ToDo list with {string} item', function(string) {
 
-});
+// });
 
-When('I click on delete button next to {string} item', function(string) {
+// When('I click on delete button next to {string} item', function(string) {
 
-});
-
-
-Then('List should be empty', function() {
-
-});
+// });
 
 
-Given('ToDo list with {string} and {string} item in order', function(string, string2) {
+// Then('List should be empty', function() {
 
-});
+// });
 
 
-When('I click on delete button next to {string} item', function(string) {
+// Given('ToDo list with {string} and {string} item in order', function(string, string2) {
 
-});
+// });
 
-Then('I should see {string} item in ToDo list', function(string) {
 
-});
+// When('I click on delete button next to {string} item', function(string) {
+
+// });
+
+// Then('I should see {string} item in ToDo list', function(string) {
+
+// });
